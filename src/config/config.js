e626458262b1abf346ada660f1d9c012e0121339
Olamibo Oluwadamilola Johnvict <johnvict20@gmail.com>
2020-06-14 16:07:47 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 
 module.exports = {
-	jwtSecret: 'offline-online-api-for-senior-programmer-is-gonna-be-wonderful',
+	jwtSecret: 'offline-online-api-secret-goes-here-make-secret-a-secret',
 	salt: bcrypt.genSaltSync(10)
 }
